04dd85abc8af6ef6d6fd8fa4f359b1aaecc3262c
Alex - laptop <penoplavaaan@gmail.com>
2021-11-19 19:24:36 +0500

    database setup finally ready

--- todo/resources/views/layouts/app.blade.php
+++ todo/resources/views/layouts/app.blade.php
@@ -54,7 +54,7 @@
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    Здравствуйте, {{ Auth::user()->name }}!! Ваш ID в системе: {{ Auth::user()->id }}
+                                    Здравствуйте, {{ Auth::user()->name }}! Ваш ID в системе: {{ Auth::user()->id }}
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
